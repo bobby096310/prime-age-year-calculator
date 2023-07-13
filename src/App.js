1c0fc88217a039025bb61e9ec3875171faf24b59
Chien-Hao Chiu <ge75cir@mytum.de>
2023-07-13 14:26:54 +0200

    V3: add error messages

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import dayjs from "dayjs";
 import {useState} from "react";
+import {useMemo} from "react";
 
 function findNextPrimeAgeYear(startingYear) {
-    const primeList = findPrimeNumbers();
+    const primeList = findPrimeNumbers(120);
     for (let prime of primeList) {
         const nextPrimeAgeYear = startingYear + prime;
         if (nextPrimeAgeYear > dayjs().year()) {
@@ -17,9 +18,9 @@ function findNextPrimeAgeYear(startingYear) {
     return "Out of Rangee";
 }
 
-function findPrimeNumbers() {
+function findPrimeNumbers(upperLimit) {
     let primeList = [];
-    for (let i=2; i<120; i++) {
+    for (let i=2; i<upperLimit; i++) {
         let isPrime = true;
         for (let j=i-1; j>1; j--) {
             if (i % j === 0) {
@@ -35,12 +36,37 @@ function findPrimeNumbers() {
 }
 
 function YearPicker({setBirthYear}) {
+    const [error, setError] = useState(null);
+
+    const errorMessage = useMemo(() => {
+        switch (error) {
+            case 'maxDate': {return 'You are not born yet...?'}
+            case 'minDate': {
+                return 'Are you sure you are this old?';
+            }
+
+            case 'invalidDate': {
+                return 'Your date is not valid';
+            }
+
+            default: {
+                return '';
+            }
+        }
+    }, [error]);
+
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DatePicker label="Choose Birth Year" views={['year']}
                         defaultValue={dayjs()}
                         minDate={dayjs().subtract(99, 'year')}
                         maxDate={dayjs()}
+                        onError={(newError) => setError(newError)}
+                        slotProps={{
+                            textField: {
+                                helperText: errorMessage,
+                            },
+                        }}
                         onChange={(value) => value ? setBirthYear(value.year()) : null}/>
         </LocalizationProvider>
     );
